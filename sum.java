fe9e66617e2664f015910edea0c63eddf4c4c49c
Prajukta <prajuktaha@gmail.com>
2025-07-01 02:54:41 +0530

    sum.java
    sum for two numbers

--- sum.java
+++ sum.java
@@ -1,9 +1,12 @@
-public class sum {
-	public static void main(String[]args) {
-		int a = 10;
-		int b = 10;
-		int sum = a+b;
-		System.out.println("sum of "+  a + " and " + b + " is: " + sum);
-	}
-
+import java.util.*;
+public class quiz1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of a:");
+        int a = sc.nextInt();
+        System.out.println("enter the value of b:");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("sum of the two numbers is:" +sum);
+    }
 }
