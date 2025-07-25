36b7ab75cba6790b693b46a7d609d115e5acda34
Prajukta <prajuktaha@gmail.com>
2025-07-26 00:06:49 +0530

    knowing substring its a demo

---
+++ Substring.java
@@ -0,0 +1,7 @@
+public class substring {
+    public static void main(String[] args) {
+        String sentence = "Hello World! and suckers";
+        String sub = sentence.substring(6, 11);
+        System.out.println("Substring: " + sub);
+    }
+}
